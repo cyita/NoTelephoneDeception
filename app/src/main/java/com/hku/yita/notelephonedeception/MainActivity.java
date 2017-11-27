cff5170cf2fef8eafb65dd5084120ddfc7b5ba4e
linlijuan <994182059@qq.com>
2017-11-27 15:25:24 +0800

    1) 获取通讯录联系人列表 2）获取来电号码

--- app/src/main/java/com/hku/yita/notelephonedeception/MainActivity.java
+++ app/src/main/java/com/hku/yita/notelephonedeception/MainActivity.java
@@ -1,13 +1,99 @@
 package com.hku.yita.notelephonedeception;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    public final static int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnIncomingCall = (Button) findViewById(R.id.button);
+        ShowContacts();
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+            }
+        }
+
+
+    }
+
+    private void ShowContacts() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+        } else {
+            readContacts();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResult) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResult[0] == PackageManager.PERMISSION_GRANTED) {
+                ShowContacts();
+            } else {
+                Toast.makeText(this, "not granted at all", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+        if (requestCode == MY_PERMISSIONS_REQUEST_READ_PHONE_STATE) {
+            if (grantResult.length > 0 && grantResult[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                Toast.makeText(this, "not granted yet", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+    }
+
+    private void readContacts() {
+        List<String> contractname = new ArrayList<String>();
+        List<String> contractnumber = new ArrayList<String>();
+        Button btn = (Button) findViewById(R.id.button);
+        ContentResolver resolver = getContentResolver();
+        Uri uri = Uri.parse("content://com.android.contacts/raw_contacts");
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+        while (cursor.moveToNext()) {
+            String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+
+            Cursor phoneCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + id, null, null);
+
+            while(phoneCursor.moveToNext()){
+
+                String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                contractname.add(name);
+                contractnumber.add(phoneNumber);
+            }
+            phoneCursor.close();
+        }
+        cursor.close();
+        btn.setText(contractname.toString()+contractnumber.toString());
+
     }
 }
---
+++ app/src/main/java/com/hku/yita/notelephonedeception/PhoneStateReceiver.java
@@ -0,0 +1,85 @@
+package com.hku.yita.notelephonedeception;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+import java.util.Date;
+
+/**
+ * Created by linlijuan on 2017/11/14.
+ */
+
+public class PhoneStateReceiver extends BroadcastReceiver {
+    private static int lastState = TelephonyManager.CALL_STATE_IDLE;
+    private static Date callStartTime;
+    private static boolean isIncoming;
+    private static String savedNumber;
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals("android.intent.action.NEW_OUTGOING_CALL")) {
+            savedNumber = intent.getExtras().getString("android.intent.extra.PHONE_NUMBER");
+
+        }
+        else{
+            String stateStr = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
+            String number = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            int state = 0;
+            if(stateStr.equals(TelephonyManager.EXTRA_STATE_IDLE)){
+                state = TelephonyManager.CALL_STATE_IDLE;
+            }
+            else if(stateStr.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
+                state = TelephonyManager.CALL_STATE_OFFHOOK;
+            }
+            else if(stateStr.equals(TelephonyManager.EXTRA_STATE_RINGING)){
+                state = TelephonyManager.CALL_STATE_RINGING;
+            }
+
+            onCallStateChanged(context, state, number);
+        }
+    }
+
+
+
+    public void onCallStateChanged(Context context, int state, String number) {
+        if(lastState == state){
+            return;
+        }
+        switch (state) {
+            case TelephonyManager.CALL_STATE_RINGING:
+                isIncoming = true;
+                callStartTime = new Date();
+                savedNumber = number;
+
+                Toast.makeText(context, "Incoming Call Ringing" , Toast.LENGTH_SHORT).show();
+                break;
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+                if(lastState != TelephonyManager.CALL_STATE_RINGING){
+                    isIncoming = false;
+                    callStartTime = new Date();
+                    Toast.makeText(context, "Outgoing Call Started" , Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+            case TelephonyManager.CALL_STATE_IDLE:
+                if(lastState == TelephonyManager.CALL_STATE_RINGING){
+                    Toast.makeText(context, "Ringing but no pickup" + savedNumber + " Call time " + callStartTime +" Date " + new Date() , Toast.LENGTH_SHORT).show();
+                }
+                else if(isIncoming){
+
+                    Toast.makeText(context, "Incoming " + savedNumber + " Call time " + callStartTime  , Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(context, "outgoing " + savedNumber + " Call time " + callStartTime +" Date " + new Date() , Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        lastState = state;
+    }
+
+}
